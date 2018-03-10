2682f2d2378ee3ef14023f442e4148749015592c
Guillaume Bouffard <guillaume@getkahoot.com>
2018-03-10 22:55:42 +0000

    Write and skip api-mock unit test

---
+++ src/__tests__/api-mock.test.js
@@ -0,0 +1,12 @@
+describe('getTopStories - ', () => {
+  xit('works when the api call is mocked with resolve', () => {
+  })
+})
+
+describe('getStory', () => {
+  xit('works when the api call is mocked with resolve', () => {
+  })
+
+  xit('works when the api call is mocked with reject', async () => {
+  })
+})
