acba3ddf443278140328eda0bbf9bb07f3b199ef
Guillaume Bouffard <guillaume@getkahoot.com>
2018-03-10 23:27:11 +0000

    Write and skip hacker-news-api unit test

---
+++ src/__tests__/hacker-news-api.test.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+import {
+  getTopStories,
+  getStory
+} from '../services/hacker-news-api';
+
+describe('getTopStories - ', () => {
+  xit('should make the correct api call', () => {
+  })
+
+  xit('should load top stories limited by the given number', () => {
+  })
+})
+
+describe('getStory - ', () => {
+  xit('should make the correct api call', () => {
+  })
+
+  xit('should resolve the story data', () => {
+  })
+
+  xit('should reject if the id does not correspond to a hacker news story', async () => {
+  })
+})
