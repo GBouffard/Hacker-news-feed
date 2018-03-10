0efe509aa0bb1e170b51eeaeb233346dcfd70abd
Guillaume Bouffard <guillaume@getkahoot.com>
2018-03-10 23:07:07 +0000

    Pass mocked unit test - getStory - works when the api call is mocked with resolve

--- src/__tests__/api-mock.test.js
+++ src/__tests__/api-mock.test.js
@@ -1,10 +1,23 @@
 import axios from 'axios';
 
 import {
-  getTopStories
-} from '../services/hacker-news-api'
+  getTopStories,
+  getStory
+} from '../services/hacker-news-api';
 
 const mockedTopStories = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const mockedStory = {
+  by: 'Guillaume',
+  descendants: 1,
+  id: 11111111,
+  kids: [22222222, 33333333],
+  score: 99,
+  time: 1520691427,
+  title: 'Guillaume is building a Hacker-news feed!',
+  type: 'story',
+  url: 'https://github.com/GBouffard'
+};
 
 describe('getTopStories - ', () => {
   it('works when the api call is mocked with resolve', () => {
@@ -18,8 +31,15 @@ describe('getTopStories - ', () => {
   })
 })
 
-describe('getStory', () => {
-  xit('works when the api call is mocked with resolve', () => {
+describe('getStory - ', () => {
+  it('works when the api call is mocked with resolve', () => {
+    axios.get = jest.fn().mockImplementation(() => Promise.resolve({ data: mockedStory }))
+
+    getStory()
+      .then(resolvedData => {
+        expect(resolvedData).toBeDefined();
+        expect(resolvedData.title).toEqual('Guillaume is building a Hacker-news feed!');
+      })
   })
 
   xit('works when the api call is mocked with reject', async () => {
--- src/services/hacker-news-api.js
+++ src/services/hacker-news-api.js
@@ -11,7 +11,25 @@ function getTopStories(limit) {
       })
   })
 }
+// 339 stories btw!
+
+function getStory (id) {
+  return new Promise((resolve, reject) => {
+    axios.get(`https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`)
+      .then(response => {
+        if (response.data) {
+          resolve(response.data)
+        } else {
+          reject('There is no Hacker news story corresponding to this ID.')
+        }
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  })
+}
 
 export {
-  getTopStories
-}
+  getTopStories,
+  getStory
+};
