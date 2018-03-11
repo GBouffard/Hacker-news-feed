89d0ba9c71c94ebafda46fcfc07bd1e15ef1ae98
Guillaume Bouffard <guillaume@getkahoot.com>
2018-03-11 20:04:30 +0000

    Add first versioning of the story-content css

--- src/components/story-content.js
+++ src/components/story-content.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../css/story-content.css';
 
 const StoryContent = ({ by, id, score, title }) => {
   return (
     <div
+      className="story-content"
       key={id}>
-      {title}  /  {by}  /  {score}
+      <div>
+        {score}
+      </div>
+      <div
+        className="story-content__title-and-author-container">
+        <div
+          className="story-content__title">
+          {title}
+        </div>
+        <div
+          className="story-content__author">
+          by: {by}
+        </div>
+      </div>
     </div>
   );
 };
